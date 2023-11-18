3ade0e4c5d4d50f60530eab05e3cf8bd1b230132
elvina-s <rammnuschka@gmail.com>
2023-11-18 22:08:32 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { data } from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+  const [person, setPerson] = useState(0);
+  const {id, name, description, age, image} = data[person];
+
+  const previousPerson = () => {
+    setPerson ((person => {
+      person--;
+      if (person < 0) {
+        return data.length - 1;
+      }
+      return person;
+    }))
+  }
+
+  const nextPerson = () => {
+    setPerson ((person => {
+      person++;
+      if (person > data.length - 1) {
+        return person = 0;
+      }
+      return person;
+    }))
+
+  }
+
+  return (<div>
+      <div className="container">
+        <img src={image} alt="photo" width="300px"></img>
+      </div>
+      <div className="container">
+        <h1>{id} - {name}</h1>
+      </div>
+      <div className="container">
+        <h2>{description}</h2>
+      </div>
+      <div className="container">
+        <h2>{age} years old</h2>
+      </div>
+      <div className="container">
+        <button onClick={previousPerson}>Previous</button>
+      </div>
+      <div className="container">
+        <button onClick={nextPerson}>Next</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
